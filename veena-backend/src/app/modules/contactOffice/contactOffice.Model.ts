2ee4f0b11d459807d5b43d8e4056fbdb4197e38e
Muskan-codebase <muskanmjvr@email.com>
2026-02-13 18:50:09 +0530

    updated contact office model

--- veena-backend/src/app/modules/contactOffice/contactOffice.Model.ts
+++ veena-backend/src/app/modules/contactOffice/contactOffice.Model.ts
@@ -1,34 +1,77 @@
 import mongoose from 'mongoose';
 import { IOffice } from './contactOffice.interface';
 
+// const officeSchema = new mongoose.Schema<IOffice>(
+//   {
+//     city: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     status: {
+//       type: String,
+//       enum: ['active', 'inactive'],
+//       default: 'active',
+//     },
+//     forex: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     address: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     phone: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//   },
+//   { timestamps: true },
+// );
+
+// export const Office = mongoose.model<IOffice>('Office', officeSchema);
+
 const officeSchema = new mongoose.Schema<IOffice>(
   {
-    city: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+    city: { type: String, required: true, trim: true },
+
     status: {
       type: String,
       enum: ['active', 'inactive'],
       default: 'active',
     },
-    forex: {
-      type: Boolean,
-      default: false,
-    },
-    address: {
+
+    forex: { type: Boolean, default: false },
+
+    address: { type: String, required: true, trim: true },
+
+    phone: { type: String, required: true, trim: true },
+
+    mapUrl: {
       type: String,
-      required: true,
       trim: true,
+      default: '',
     },
-    phone: {
+
+    officeState: {
       type: String,
-      required: true,
-      trim: true,
+      enum: ['open', 'closed'],
+      default: 'open',
     },
+
+    timings: [
+      {
+        day: String,
+        open: String,
+        close: String,
+        closed: Boolean,
+      },
+    ],
   },
   { timestamps: true },
 );
 
 export const Office = mongoose.model<IOffice>('Office', officeSchema);
+
